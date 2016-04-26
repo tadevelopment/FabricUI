8cbcd9b80a35d829c511a61c414e40212034359d
jcouet <couet.julien@gmail.com>
2016-04-26 13:55:20 -0400

    Disable DCC build

--- SceneHub/SHGLScene.h
+++ SceneHub/SHGLScene.h
@@ -27,7 +27,7 @@ class SHGLScene {
     SHGLScene is a helper class wrapping :kl-ref:`SHGLScene`.
     It allows easy and safe access in C++ to the KL methods.
 
-    When possible, the logic is write most of the code in :kl-ref:`SHGLScene`, 
+    When possible, the logic is to write most of the code in :kl-ref:`SHGLScene`.
   */
 
 
