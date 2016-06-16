9774e31260d4cdbb6c1179216b5f5345077dfa6e
Peter Zion <peter.zion@gmail.com>
2016-06-15 21:12:08 -0400

    blocks: fix OS X build breaks

--- SceneHub/DFG/SHDFGBinding.cpp
+++ SceneHub/DFG/SHDFGBinding.cpp
@@ -167,7 +167,7 @@ void SHDFGBinding::onDriveNodeInputPorts(RTVal event) {
         QString subExecParentPath;
         for(int i=0; i<list.size()-2; ++i) 
           subExecParentPath += list[i] + ".";
-        subExecParentPath.left(subExecParentPath.lastIndexOf("."));
+        // subExecParentPath.left(subExecParentPath.lastIndexOf("."));
         if(subExecParentPath == "") 
           subExecParentPath = ".";
 
