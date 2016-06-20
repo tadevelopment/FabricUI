0737e9ca3d86db4fb4660bb3b502f166a0eaccf5
Eric <Eric>
2016-06-20 15:02:25 +0200

    FE-6075 make int slider behave the same as float slider

--- ValueEditor/IntSlider.cpp
+++ ValueEditor/IntSlider.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "IntSlider.h"
+#include <QtGui/QStyleOptionSlider>
 
 using namespace FabricUI::ValueEditor;
 
@@ -12,6 +13,48 @@ IntSlider::IntSlider( QWidget *parent )
 
   // Default to horizontal orientation
   setOrientation( Qt::Horizontal );
+}
+
+void IntSlider::mousePressEvent( QMouseEvent *event )
+{
+  // [FE-] taken from FabricUI::ValueEditor::DoubleSlider::mousePressEvent().
+  QStyleOptionSlider opt;
+  initStyleOption( &opt );
+  QRect sr = style()->subControlRect( QStyle::CC_Slider, &opt, QStyle::SC_SliderHandle, this );
+
+  if (event->button() == Qt::LeftButton &&
+        sr.contains( event->pos() ) == false)
+  {
+    int max = maximum();
+
+    int newVal = 0;
+    if ( orientation() == Qt::Vertical )
+    {
+      int h = height();
+      if ( h > 1 )
+      {
+        --h;
+        newVal += (max * (h - event->y()) + h/2) / h;
+      }
+    }
+    else
+    {
+      int w = width();
+      if ( w > 1 )
+      {
+        --w;
+        newVal += (max * event->x() + w/2) / w;
+      }
+    }
+
+    if ( invertedAppearance() )
+      setValue( max - newVal );
+    else
+      setValue( newVal );
+
+    event->accept();
+  }
+  QSlider::mousePressEvent( event );
 }
 
 void IntSlider::wheelEvent( QWheelEvent *event )
