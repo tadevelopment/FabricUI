e734a08181b3d16b0e24e87bc3e086719503058e
jcouet <couet.julien@gmail.com>
2016-10-17 13:32:39 -0400

    Improve delete_xx_port unit-tests outputs

--- Test/ScriptEditor/delete_in_port.py
+++ Test/ScriptEditor/delete_in_port.py
@@ -1,7 +1,18 @@
 binding_exec = binding.binding.getExec()
 print "Number of Port before adding : " + str(binding_exec.getExecPortCount())
+for i in range(binding_exec.getExecPortCount()):
+  portName = binding_exec.getExecPortName(i)
+  print "Port  " + str(portName) + " type " + str(binding_exec.getPortType(portName))
+
 binding.addPort("", "inPort", "in", "Float32", "", "", "{\n  \"uiRange\" : \"\",\n  \"uiHardRange\" : \"\"\n  }")
 print "Number of Port after adding : " + str(binding_exec.getExecPortCount())
+for i in range(binding_exec.getExecPortCount()):
+  portName = binding_exec.getExecPortName(i)
+  print "Port  " + str(portName) + " type " + str(binding_exec.getPortType(portName))
+
 binding.removePort("", "inPort")
 print "Number of Port after removing : " + str(binding_exec.getExecPortCount())
+for i in range(binding_exec.getExecPortCount()):
+  portName = binding_exec.getExecPortName(i)
+  print "Port  " + str(portName) + " type " + str(binding_exec.getPortType(portName))
 
