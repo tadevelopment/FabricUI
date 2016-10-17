cdb29d4af551fbc301dc520d9fb87001da4877d4
jcouet <couet.julien@gmail.com>
2016-10-17 13:29:55 -0400

    Improve create_xx_port unit-tests outputs

--- Test/ScriptEditor/create_in_port.py
+++ Test/ScriptEditor/create_in_port.py
@@ -1,4 +1,12 @@
 binding_exec = binding.binding.getExec()
 print "Number of Port before adding : " + str(binding_exec.getExecPortCount())
+for i in range(binding_exec.getExecPortCount()):
+  portName = binding_exec.getExecPortName(i)
+  print "Port  " + str(portName) + " type " + str(binding_exec.getPortType(portName))
+
 binding.addPort("", "inPort", "in", "Float32", "", "", "{\n  \"uiRange\" : \"\",\n  \"uiHardRange\" : \"\"\n  }")
+
 print "Number of Port after adding : " + str(binding_exec.getExecPortCount())
+for i in range(binding_exec.getExecPortCount()):
+  portName = binding_exec.getExecPortName(i)
+  print "Port  " + str(portName) + " type " + str(binding_exec.getPortType(portName))
