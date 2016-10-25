b489c89c18898dbbc1d5afb2def8159adc40fccd
jcouet <couet.julien@gmail.com>
2016-10-25 12:04:07 -0400

    Move test_canvas_ui.py and test_canvas_scripteditor.py to the Test folfer (FE-7358)

--- Test/ScriptEditor/create_empty_graph.py
+++
@@ -1,5 +0,0 @@
-binding.addGraph("", "graph", 30.0, 383.0)
-# Result: graph
-binding_exec = binding.binding.getExec()
-for i in range(binding_exec.getNodeCount()):
-  print binding_exec.getNodeName(i)
--- Test/ScriptEditor/graph_name.py
+++
@@ -1,5 +0,0 @@
-binding.addGraph("", "graph", 867.0, 218.0)
-binding_exec = binding.binding.getExec()
-print binding_exec.getNodeName(0)
-binding.editNode("", "graph", "newGraph", "{}", "{\n  \"uiTooltip\" : \"\",\n  \"uiDocUrl\" : \"\",\n  \"uiNodeColor\" : \"{\\n  \\\"r\\\" : 90,\\n  \\\"g\\\" : 37,\\n  \\\"b\\\" : 143\\n  }\",\n  \"uiTextColor\" : \"{\\n  \\\"r\\\" : 255,\\n  \\\"g\\\" : 213,\\n  \\\"b\\\" : 0\\n  }\",\n  \"uiHeaderColor\" : \"\"\n  }")
-print binding_exec.getNodeName(0)
