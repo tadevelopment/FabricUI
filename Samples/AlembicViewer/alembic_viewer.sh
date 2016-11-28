10cf59a144d355e47ad742dbe22c32cd63d6d63d
Eric Thivierge <ethivierge@gmail.com>
2016-11-28 12:35:55 -0500

    Updated alembic_viewer.sh to convert *nix path to windows path when on windows to be able to source alembic_viewer.sh from Windows PUBLISH:{alembic_viewer.sh now converts *nix path to windows path so it can be sourced on Windows systems}

--- Samples/AlembicViewer/alembic_viewer.sh
+++ Samples/AlembicViewer/alembic_viewer.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+unix_to_windows_path()
+{
+  echo "$@" | sed 's/^\/\(.\)\//\1:\\/' | sed 's/\//\\/g'
+}
+
 echo "========================"
 echo "Launching Alembic Viewer"
 echo "========================"
 echo ""
 
-BASEDIR=$(dirname "$0")
-FABRIC_DIR=$BASEDIR/../../..
+source ../../../environment.sh
 
-source $FABRIC_DIR/environment.sh
+ALEMBICVIEWERPYPATH=$FABRIC_DIR/Samples/Python
 
-PYTHONPATH=$BASEDIR/../:$PYTHONPATH
+FABRIC_OS=$(uname -s)
+if [[ "$FABRIC_OS" == *W32* ]] || [[ "$FABRIC_OS" == *W64* ]]; then
+  PYTHONPATH="$(unix_to_windows_path $ALEMBICVIEWERPYPATH);$PYTHONPATH"
+else
+  PYTHONPATH=$ALEMBICVIEWERPYPATH:$PYTHONPATH
+fi
 export PYTHONPATH
 
-python $BASEDIR/alembic_viewer.py
+python $FABRIC_DIR/Samples/Python/AlembicViewer/alembic_viewer.py
