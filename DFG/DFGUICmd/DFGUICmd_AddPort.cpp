a61b230371debed891f1d5f694f0d973a4db1705
Eric Mootz <eric@mootzoid.com>
2016-12-20 15:44:24 +0100

    FE-7700 re-arrange things

--- DFG/DFGUICmd/DFGUICmd_AddPort.cpp
+++ DFG/DFGUICmd/DFGUICmd_AddPort.cpp
@@ -125,35 +125,13 @@ FTL::CStrRef DFGUICmd_AddPort::invoke(
       exec.getPortType( portToConnectPath.c_str() );
     if ( portToConnectNodePortType == FabricCore::DFGPortType_In )
     {
+      // copy the metadata.
+
       FTL::CStrRef::Split split = portToConnectPath.rsplit('.');
       std::string portToConnectNodeName = split.first;
       FTL::CStrRef portToConnectName = split.second;
       if ( !portToConnectNodeName.empty() )
       {
-        bool setPortAsPersistable = false;  // [FE-7700]
-
-        FTL::CStrRef resolvedType =
-          exec.getPortResolvedType( portToConnectPath.c_str() );
-        if ( !resolvedType.empty() )
-        {
-          FabricCore::RTVal defaultValue =
-            exec.getPortResolvedDefaultValue(
-              portToConnectPath.c_str(),
-              resolvedType.c_str()
-              ).clone();  // [FE-7700]
-          if ( defaultValue.isValid() )
-          {
-            if ( execPath.empty() )
-            {
-              binding.setArgValue( portName.c_str(), defaultValue, true );
-              setPortAsPersistable = true;
-            }
-            else
-              exec.setPortDefaultValue( portName.c_str(), defaultValue, true );
-            ++coreUndoCount;
-          }
-        }
-
         static unsigned const metadatasToCopyCount = 5;
         char const *metadatasToCopy[metadatasToCopyCount] =
         {
@@ -203,19 +181,34 @@ FTL::CStrRef DFGUICmd_AddPort::invoke(
           }
         }
 
-        if (setPortAsPersistable)
+        // set the new port's value
+
+        FTL::CStrRef resolvedType =
+          exec.getPortResolvedType( portToConnectPath.c_str() );
+        if ( !resolvedType.empty() )
         {
-          // set the port as persistable. This was done in the setArgValue()
-          // call above (see implementation of DFGUICmd_SetArgValue::invoke()),
-          // but copying the metadata afterwards possibly removed or reset
-          // the DFG_METADATA_UIPERSISTVALUE metadata, so we set it again here.
-          exec.setExecPortMetadata(
-            portName.c_str(),
-            DFG_METADATA_UIPERSISTVALUE,
-            "true",
-            true
-            );
-          ++coreUndoCount;
+          FabricCore::RTVal defaultValue =
+            exec.getPortResolvedDefaultValue(
+              portToConnectPath.c_str(),
+              resolvedType.c_str()
+              ).clone();
+          if ( defaultValue.isValid() )
+          {
+            if ( execPath.empty() )
+            {
+              binding.setArgValue( portName.c_str(), defaultValue, true );
+              // [FE-7700] automatically set root ports as "persistable".
+              getBinding().getExec().setExecPortMetadata(
+                portName.c_str(),
+                DFG_METADATA_UIPERSISTVALUE,
+                "true",
+                false
+                );
+            }
+            else
+              exec.setPortDefaultValue( portName.c_str(), defaultValue, true );
+            ++coreUndoCount;
+          }
         }
       }
     }
