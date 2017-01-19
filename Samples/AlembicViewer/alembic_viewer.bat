a4ee7a594d90059135d1217c4f2e122ed891e634
Peter Zion <peter.zion@gmail.com>
2017-01-19 10:46:09 -0500

    Fixes for batch file and shell script file quoting

--- Samples/AlembicViewer/alembic_viewer.bat
+++ Samples/AlembicViewer/alembic_viewer.bat
@@ -7,14 +7,14 @@ REM ==================================
 REM Calls the Fabric Environment .bat
 REM ==================================
 
-CALL %cd%\..\..\..\environment.bat
+CALL "%cd%\..\..\..\environment.bat"
 
 REM ==================================
 REM Adds the parent directory to the
 REM PYTHONPATH environment variable.
 REM ==================================
 
-set PYTHONPATH=%PYTHONPATH%;%cd%\..;
+set "PYTHONPATH=%PYTHONPATH%;%cd%\.."
 
 REM ==================================
 REM Launches the Python application.
