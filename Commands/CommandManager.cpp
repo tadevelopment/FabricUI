e44d318276454fcecd6fe22d049b0a6fbb719ff8
Julien Couet <couet.julien@gmail.com>
2017-03-01 16:15:40 -0500

    Rename BaseCommandManager to CommandManager

--- Commands/CommandManager.cpp
+++ Commands/CommandManager.cpp
@@ -3,31 +3,86 @@
 //
 
 #include <string>
+#include "KLCommand.h"
 #include "CommandManager.h"
-#include "CommandRegistry.h"
+#include "KLScriptableCommand.h"
+#include "BaseScriptableCommand.h"
 
 using namespace FabricUI;
 using namespace Commands;
+using namespace FabricCore;
 
-CommandManager::CommandManager() 
-  : BaseCommandManager() 
+bool CommandManager::s_instanceFlag = false;
+CommandManager* CommandManager::s_cmdManager = 0;
+
+CommandManager::CommandManager(
+  Client client) 
+  : QObject()
+  , m_client(client)
 {
+  if(s_instanceFlag)
+    throw("CommandManager::CommandManager, CommandManager singleton has already been created");
+  
+  try 
+  {
+    m_klCmdManager = RTVal::Create(
+      m_client, 
+      "CommandManager", 
+      0, 
+      0);
+
+    m_klCmdManager = m_klCmdManager.callMethod(
+      "CommandManager", 
+      "getCommandManager", 
+      0, 
+      0);
+  }
+
+  catch(Exception &e)
+  {
+    printf(
+      "CommandManager::CommandManager: exception: %s\n", 
+      e.getDesc_cstr());
+  }
+
+  /// Registers our-self, needed in python.
+  QObject::connect(
+    CommandRegistry::GetCommandRegistry(),
+    SIGNAL(
+      commandRegisteredCallback(
+        const QString &,
+        const QString &,
+        const QString &)
+      ),
+    this,
+    SLOT(
+      onCommandRegistered(
+        const QString &,
+        const QString &,
+        const QString &)
+      )
+    );
+  
+  // Set the pointer of the CommandManager singleton
+  // equal to this instance of CommandManager.
+  s_cmdManager = this;
+  s_instanceFlag = true;
 }
 
 CommandManager::~CommandManager() 
 {
+  clear();
+  s_instanceFlag = false;
 }
 
-BaseCommandManager* CommandManager::GetCommandManager()
+CommandManager* CommandManager::GetCommandManager()
 {
-  BaseCommandManager *manager = BaseCommandManager::GetCommandManager();
-  if (!manager)
-  {
-    manager = new CommandManager();
-    BaseCommandManager::SetCommandManagerSingleton(manager);
-  }
-  
-  return manager;
+  if(!s_instanceFlag)
+    throw(
+      std::string("CommandManager::CommandManager, the manager is null")
+    );
+
+  return s_cmdManager;
 }
 
 BaseCommand* CommandManager::createCommand(
@@ -39,7 +94,7 @@ BaseCommand* CommandManager::createCommand(
   {  
     BaseCommand *cmd = CommandRegistry::GetCommandRegistry()->createCommand(cmdName);
     
-    if( args.size() > 0 ) 
+    if(args.size() > 0) 
       checkCommandArgs(cmd, args);
 
     if (doCmd) 
@@ -57,4 +112,460 @@ BaseCommand* CommandManager::createCommand(
   }
 
   return 0;
+}
+
+void CommandManager::doCommand(
+  BaseCommand *cmd) 
+{
+  if(!cmd) 
+    throw(std::string("CommandManager::doCommand, Command is null"));
+ 
+  bool subCmd = m_undoStack.size() != 0 && !m_undoStack[m_undoStack.size() - 1].succeeded;
+  if(!subCmd) 
+  {
+    // Clear all the redo-able commands
+    clearRedoStack();
+    // push a new one
+    pushTopCommand(cmd, false);
+  }
+
+  if (!cmd->doIt())
+  {
+    //Use the name of the top-level cmd
+    BaseCommand* cmdForErrorLog = (cmd != 0) ? cmd : m_undoStack[m_undoStack.size() - 1].topLevelCmd;
+    throw(
+      std::string(
+        QString(
+          "CommandManager::doCommand, error doing command '" + cmdForErrorLog->getName() + "'"
+        ).toUtf8().constData() 
+     )
+   );
+  } 
+
+  else if (subCmd && cmd->canUndo())
+    pushLowCommand(cmd);
+ 
+  else
+    m_undoStack[m_undoStack.size() - 1].succeeded = true;
+}
+
+void CommandManager::undoCommand() 
+{
+  if(m_undoStack.size() == 0)
+  {
+    printf("CommandManager::undoCommand: nothing to undo");
+    return;
+  }
+
+  StackedCommand stackedCmd = m_undoStack[m_undoStack.size() - 1];
+  BaseCommand *top = stackedCmd.topLevelCmd;
+
+  int lowLevelCmdsCount = int(stackedCmd.lowLevelCmds.size());
+  if(lowLevelCmdsCount > 0)
+  {
+    for(int i = lowLevelCmdsCount; i--;)
+    {
+      BaseCommand *low = stackedCmd.lowLevelCmds[i];
+      if(!low->undoIt())
+      {
+        for(int j = i+1; j<lowLevelCmdsCount; ++j)
+        {
+          BaseCommand *low_ = stackedCmd.lowLevelCmds[j];
+          low_->redoIt();
+        }
+
+        throw(
+          std::string(
+            QString(
+              "CommandManager::undoCommand, error undoing command, top: '" + 
+              top->getName() + "', low: '" + low->getName() + "'"
+            ).toUtf8().constData() 
+         )
+       );
+      }
+    }
+  }
+  
+  else if(!top->undoIt())
+    throw(
+      std::string(
+        QString(
+          "CommandManager::undoCommand, error undoing command : " + 
+          top->getName()
+        ).toUtf8().constData() 
+     )
+   );
+
+  m_undoStack.pop_back();
+  m_redoStack.push_back(stackedCmd);
+}
+
+void CommandManager::redoCommand() 
+{
+  if(m_redoStack.size() == 0)
+  {
+    printf("CommandManager::redoCommand: nothing to redo");
+    return;
+  }
+
+  StackedCommand stackedCmd = m_redoStack[m_redoStack.size() - 1];
+  BaseCommand *top = stackedCmd.topLevelCmd;
+
+  int lowLevelCmdsCount = int(stackedCmd.lowLevelCmds.size());
+  if(lowLevelCmdsCount > 0) 
+  {
+    for (int i = 0; i < lowLevelCmdsCount; ++i)
+    {
+      BaseCommand *low = stackedCmd.lowLevelCmds[i]; 
+      if(!low->redoIt())
+      {
+        for(int j = i; j--;)
+        {
+          BaseCommand *low_ = stackedCmd.lowLevelCmds[j];
+          low_->undoIt();
+        }
+
+        throw(
+          std::string(
+            QString(
+              "CommandManager::redoCommand, error redoing command, top: '" + 
+              top->getName() + "', low: '" + low->getName() + "'"
+            ).toUtf8().constData()
+          )
+        );
+      }
+    }
+  }
+
+  else if(!top->redoIt())
+    throw(
+      std::string(
+        QString(
+          "CommandManager::redoCommand, error redoing command: '" + 
+          top->getName() + "'"
+        ).toUtf8().constData() 
+     )
+   );
+ 
+  m_redoStack.pop_back();
+  m_undoStack.push_back(stackedCmd);
+}
+
+void CommandManager::clear() 
+{
+  // Clear the KL command manager
+  try 
+  {
+    m_klCmdManager.callMethod(
+      "", 
+      "clear", 
+      0, 
+      0);
+  }
+
+  catch(Exception &e)
+  {
+    printf(
+      "CommandManager::clear: exception: %s\n", 
+      e.getDesc_cstr());
+  }
+
+  // Clear our-self
+  clearRedoStack();
+  clearCommandStack(m_undoStack);
+}
+
+int CommandManager::count()
+{
+  return int(m_redoStack.size() + m_undoStack.size());
+}
+
+int CommandManager::getStackIndex()
+{
+  return int(m_undoStack.size() - 1);
+}
+
+RTVal CommandManager::getKLCommandManager()
+{
+  return m_klCmdManager;
+}
+
+Client CommandManager::getFabricClient()
+{
+  return m_client;
+}
+
+void CommandManager::synchronizeKL() 
+{
+  // Get the number of KL commands 
+  // in the KL manager undo stack
+  int klCmdCount = 0;
+  try
+  {
+    klCmdCount = m_klCmdManager.callMethod(
+      "SInt32", 
+      "getStackIndex", 
+      0, 
+      0).getSInt32();
+
+    klCmdCount ++;
+  }
+
+  catch(Exception &e)
+  {
+    printf(
+      "CommandManager::synchronizeKL: exception: %s\n", 
+      e.getDesc_cstr());
+  }
+
+  // Get the number of undo KL commands 
+  // in this manager
+  int cppKLCmdCount = 0;
+  for(unsigned int i=0; i<m_undoStack.size(); ++i)
+  {
+    KLCommand *cmd = dynamic_cast<KLCommand *>(m_undoStack[i].topLevelCmd);
+    KLScriptableCommand *scriptCmd = dynamic_cast<KLScriptableCommand *>(m_undoStack[i].topLevelCmd);
+    cppKLCmdCount += (cmd || scriptCmd) ? 1 : 0;
+  } 
+
+  // Synchronize our stack with KL
+  if (cppKLCmdCount < klCmdCount)
+  {
+    try
+    {
+      for(unsigned int i=cppKLCmdCount; i<klCmdCount; ++i)
+      {
+        BaseCommand *cmd = 0;
+
+        RTVal cmdIndex = RTVal::ConstructUInt32(m_client, i);
+
+        // Gets the KL command from the KL manager. 
+        // Check if it's a scriptable command
+        RTVal klCmd = m_klCmdManager.callMethod(
+          "BaseScriptableCommand", 
+          "getCommandAtIndex", 
+          1, 
+          &cmdIndex);
+
+        if(!klCmd.isNullObject())
+          cmd = new KLScriptableCommand(klCmd);
+        
+        // if not, it's a simple command.
+        else
+        {
+          klCmd = m_klCmdManager.callMethod(
+            "Command", 
+            "getCommandAtIndex", 
+            1, 
+            &cmdIndex);
+
+          cmd = new KLCommand(klCmd);
+        }
+
+        // Push the command
+        pushTopCommand(cmd, true);
+      } 
+    }
+
+    catch(Exception &e)
+    {
+      printf(
+        "CommandManager::synchronizeKL: exception: %s\n", 
+        e.getDesc_cstr());
+    }
+  }
+
+  // !! Problem, KL and C++ command managers are out of synch
+  else if(cppKLCmdCount > klCmdCount)
+    throw(
+      std::string(
+        QString(
+          "CommandManager::synchronizeKL: the KL and C++ command managers are out of synch"
+        ).toUtf8().constData() 
+      )
+    );
+}
+
+QString CommandManager::getContent()
+{
+  QString res = 
+    QString(
+      "--> Command Manager - size:"+ QString::number(count()) + ", index:" + QString::number(getStackIndex()) + 
+      ", undo:"+  QString::number(m_undoStack.size()) +  ", redo:" + QString::number(m_redoStack.size()) + "\n"
+    );
+  res += getStackContent("Undo", m_undoStack);
+  res += getStackContent("Redo", m_redoStack);
+
+  try 
+  {
+    res += "\n" +
+      QString(
+        m_klCmdManager.callMethod(
+          "String", 
+          "getContent", 
+          0, 
+          0).getStringCString()
+      );
+  }
+
+  catch(Exception &e)
+  {
+    printf(
+      "CommandManager::getContent: exception: %s\n", 
+      e.getDesc_cstr());
+  }
+
+  return res;
+}
+
+void CommandManager::checkCommandArgs(
+  BaseCommand *cmd,
+  const QMap<QString, QString> &args)
+{
+  if(args.size() > 0) 
+  {
+    BaseScriptableCommand* scriptCommand = dynamic_cast<BaseScriptableCommand*>(cmd);
+    
+    if(!scriptCommand) 
+      throw(
+        std::string(
+          QString(
+            "CommandManager::checkCommandArgs, command '" + cmd->getName() + "' is created with args, but is not implementing the ScriptableCommand interface"
+          ).toUtf8().constData() 
+        )
+      );
+
+    // Try to set the arg even if not part of the specs, 
+    // some commands might require this
+    QMapIterator<QString, QString> ite(args);
+    while (ite.hasNext()) 
+    {
+      ite.next();
+      scriptCommand->setArg(ite.key(), ite.value());
+    }
+
+    scriptCommand->validateSetArgs();
+  }
+}
+
+void CommandManager::onCommandRegistered(
+  const QString &cmdName,
+  const QString &cmdType,
+  const QString &implType)
+{
+}
+
+void CommandManager::clearRedoStack() 
+{
+  try 
+  {
+    m_klCmdManager.callMethod(
+      "", 
+      "clearRedoStack", 
+      0, 
+      0);
+  }
+
+  catch(Exception &e)
+  {
+    printf(
+      "CommandManager::clearRedoStack: exception: %s\n", 
+      e.getDesc_cstr());
+  }
+
+  clearCommandStack(m_redoStack);
+}
+
+void CommandManager::clearCommandStack(
+  QList<StackedCommand> &stackedCmds) 
+{
+  for (int i = 0; i < stackedCmds.size(); ++i)
+  {
+    StackedCommand stackedCmd = stackedCmds[i];
+
+    // Check that the command hasn't been deleted before,
+    // in the case we don't own it (Python wrapping)
+    if(stackedCmd.topLevelCmd != 0)
+    { 
+      delete stackedCmd.topLevelCmd;
+      stackedCmd.topLevelCmd = 0;
+
+      for (int j = 0; j < stackedCmd.lowLevelCmds.size(); ++j)
+      {
+        if(stackedCmd.lowLevelCmds[j] != 0)
+        { 
+          delete stackedCmd.lowLevelCmds[j];
+          stackedCmd.topLevelCmd = 0;
+        }
+      }
+    }
+  }
+
+  stackedCmds.clear();
+}
+
+void CommandManager::pushTopCommand(
+  BaseCommand *cmd,
+  bool succeeded) 
+{ 
+  StackedCommand stackedCmd;
+  stackedCmd.topLevelCmd = cmd;
+  stackedCmd.succeeded = succeeded;
+  m_undoStack.push_back(stackedCmd);
+  commandPushed(cmd, false);
+}
+
+void CommandManager::pushLowCommand(
+  BaseCommand *cmd) 
+{ 
+  m_undoStack[m_undoStack.size() - 1].lowLevelCmds.push_back(cmd);
+  commandPushed(cmd, true);
+}
+
+void CommandManager::commandPushed(
+  BaseCommand *cmd,
+  bool isLowCmd) 
+{ 
+  emit commandPushedCallback(
+    cmd,
+    isLowCmd);
+}
+
+QString CommandManager::getStackContent(
+  const QString& stackName, 
+  const QList<StackedCommand>& stack)
+{
+  int offset = stackName == "Redo" ? m_undoStack.size() : 0;
+  QString inf = stackName == "Redo" ? "-" : "+";
+
+  QString res;
+  for (int i = 0; i < stack.size(); ++i)
+  {
+    StackedCommand stackedCmd = stack[i];
+
+    BaseCommand *top = stackedCmd.topLevelCmd;
+    BaseScriptableCommand *scriptableTop = dynamic_cast<BaseScriptableCommand *>(top);
+    
+    QString desc = scriptableTop 
+      ? top->getName() + "\n" + scriptableTop->getArgsDescription() 
+      : top->getName();
+
+    res += QString(inf + "[" + QString::number(offset + i) + "] " + desc);
+    res += "\n";
+
+    for (int j = 0; j < stackedCmd.lowLevelCmds.size(); ++j)
+    {
+      BaseCommand *low = stackedCmd.lowLevelCmds[j];
+      BaseScriptableCommand *scriptableLow = dynamic_cast<BaseScriptableCommand *>(low);
+      
+      QString desc = scriptableLow ? 
+        low->getName() + "\n" + scriptableLow->getArgsDescription() : 
+        low->getName();
+
+      res += QString("  "+ inf + "[" + QString::number(j) + "] " + desc);
+      res += "\n";
+    }
+  }
+
+  return res;
 }
--- Commands/CommandManager.h
+++ Commands/CommandManager.h
@@ -2,43 +2,209 @@
 // Copyright (c) 2010-2017 Fabric Software Inc. All rights reserved.
 //
 
-#ifndef __UI_COMMAND_MANAGER__
-#define __UI_COMMAND_MANAGER__
+#ifndef __UI_BASE_COMMAND_MANAGER__
+#define __UI_BASE_COMMAND_MANAGER__
 
-#include "BaseCommandManager.h"
- 
+#include <QMap>
+#include <QList>
+#include <QObject>
+#include <QString>
+#include <FabricCore.h>
+#include "BaseCommand.h"
+#include "CommandRegistry.h"
+
+// Need to use a typedef because gcc doesn't support templated default arguments:
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=39426 
+typedef QMap<QString, QString> QMapString;
+
 namespace FabricUI {
 namespace Commands {
 
-class CommandManager : public BaseCommandManager
+class CommandManager : public QObject
 {
   /**
-    CommandManager specializes BaseCommandManager in C++.
-    It is a singleton and should not be created directly.
-    - Get the manager: BaseCommandManager *cmdManager = CommandManager::GetCommandManager()
-  
-    - Register a C++ command: CommandFactory<cmdType>::RegisterCommand(cmdName, userData);
+    CommandManager contains and manages the command undo-redo stack. It's in charge 
+    of creating the commands, executing and undo-redoing them. It has a reference to 
+    tke KL command manager so it can create KL commands. However, the C++ manager is not
+    automatically ynchronized with the KL manager. If commands are added to the KL manager
+    directly, the C++ manager needs explicitly synchronizes with it. The manager can only 
+    create commands (KL/C++/Python) registered in the command registry.
     
-    - Create a command (Python/C++): BaseCommand *cmd = cmdManager->createCommand(cmdName);
+    CommandManager can register 'CommandManagerCallback' that are informed when
+    commands have been added to the stack. It's mainly used to attached the command
+    to external stacks.
+
+    CommandManager is specialized depending on the command registration system (C++/Phyton), 
+    see CommandManager.h(cpp) for the C++ implementation, CommandManager_Python.h(cpp)
+    and Python/Canvas/CommandManager.py for the Python one. The manager is shared between 
+    C++ and Python, so commands defined in Python can be created from C++, and vice versa.
+    
+    The manager sets it-self as a singleton when it's constructed:
+    - Create the manager: CommandManager cmdManager(fabricClient);
+  
+    - Get the manager: CommandManager *cmdManager = CommandManager::GetCommandManager();
+
+    Usage:
+    - Create a command (C++/KL/Python): BaseCommand *cmd = cmdManager->createCommand(cmdName, args, doIt);
+    
+    - Execute a command (C++/KL/Python): cmdManager->doCommand(cmd);
+    
+    - Proces undo: cmdManager->undoCommand();
+
+    - Proces redo: cmdManager->redoCommand();
+
+    - Synchronize with the KL manager: cmdManager->synchronizeKL();
   */
+  Q_OBJECT
 
   public:
-    CommandManager();
+    CommandManager(
+      FabricCore::Client client
+      );
 
     virtual ~CommandManager();
-
-    /// Gets the reference to the manager singleton.
-    /// Creates it if needed (if not created in Python)
-    static BaseCommandManager* GetCommandManager();
-
-    /// Implementation of BaseCommandManager
-    BaseCommand* createCommand(
+  
+    /// Gets the manager singleton.
+    /// Thows an error if the manager has not been created.
+    static CommandManager* GetCommandManager();
+   
+    /// Creates and executes a command (if doCmd == true).
+    /// If executed, the command is added to the manager stack.
+    /// Throws an exception if an error occurs.
+    /// To override depending of the registration mechanism.
+    virtual BaseCommand* createCommand(
       const QString &cmdName, 
       const QMap<QString, QString> &args = QMapString(), 
-      bool doCmd = true);
-};
+      bool doCmd = true
+      );
 
+    /// Executes a command and adds it to the undo stack.
+    /// Throws an exception if an error occurs.
+    void doCommand(
+      BaseCommand* cmd
+      );
+
+    /// Undoes the current command.
+    /// Throws an exception if an error occurs.
+    void undoCommand();
+
+    /// Redoes the next command.
+    /// Throws an exception if an error occurs.
+    void redoCommand();
+
+    /// Clears all the commands.
+    virtual void clear();
+
+    /// Returns the number of commands.
+    int count();
+
+    /// Gets the current index (next command to undo).
+    /// If -1 is returns, there is no command to undo.
+    int getStackIndex();
+
+    /// Gets the KL comand manager.
+    FabricCore::RTVal getKLCommandManager();
+
+    /// Gets the FabricCore client.
+    FabricCore::Client getFabricClient();
+    
+    /// Synchronizes with the KL manager.
+    /// To call explicitly after KL commands 
+    /// have been created from KL.
+    void synchronizeKL();
+
+    /// Gets the stack content as a string.
+    /// Used for debugging.
+    QString getContent();
+
+  signals:
+    /// Emitted when a command has 
+    /// been pushed to the stack.
+    void commandPushedCallback(
+      BaseCommand *cmd,
+      bool isLowCmd
+      );
+
+  protected slots:
+    /// Called when a command has been registered,
+    /// needed by the python implementation.
+    virtual void onCommandRegistered(
+      const QString &cmdName,
+      const QString &cmdType,
+      const QString &implType
+    );
+
+  protected:
+    /// Checks the command arguments before doing it.
+    /// Throws an exception if an error occurs.
+    void checkCommandArgs(
+      BaseCommand *cmd,
+      const QMap<QString, QString> &args
+      );
+
+    /// Inform a command has been pushed. 
+    /// Needed by the python implementation.
+    virtual void commandPushed(
+      BaseCommand *cmd,
+      bool isLowCmd = false
+      );
+
+    /// Clears the redo stack.
+    virtual void clearRedoStack();
+
+    /// Structure containing a command (top) and
+    /// its sub-commands (low) in a flat array.
+    struct StackedCommand 
+    {
+      bool succeeded;
+      BaseCommand* topLevelCmd;
+      QList<BaseCommand*> lowLevelCmds;
+
+      StackedCommand() 
+      {
+        topLevelCmd = 0;
+        succeeded = false;
+      }
+    };
+
+    /// Undo-reso stacks
+    QList<StackedCommand> m_undoStack, m_redoStack;
+
+  private:
+    /// Clears a specific stack.
+    void clearCommandStack(
+      QList<StackedCommand> &stackedCmd
+      );
+
+    /// Pushes a command.
+    void pushTopCommand(
+      BaseCommand *cmd,
+      bool succeeded = false
+      );
+
+    /// Pushes a sub-command.
+    void pushLowCommand(
+      BaseCommand *cmd
+      );
+ 
+    /// Gets a specific stack content 
+    /// as a string, used for debugging.
+    QString getStackContent(
+      const QString& stackName, 
+      const QList<StackedCommand> &stack
+      );
+
+    /// KL command manager.
+    FabricCore::RTVal m_klCmdManager;
+    /// Fabric client.
+    FabricCore::Client m_client;
+    /// CommandManager singleton, set from Constructor.
+    static CommandManager *s_cmdManager;
+    /// Check if the singleton has been set.
+    static bool s_instanceFlag;
+};
+ 
 } // namespace Commands
 } // namespace FabricUI
 
-#endif // __UI_COMMAND_MANAGER__
+#endif // __UI_BASE_COMMAND_MANAGER__
