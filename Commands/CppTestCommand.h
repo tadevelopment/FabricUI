0d41bc15eb4bcf0648246324d23b9e89426947f3
Julien Couet <couet.julien@gmail.com>
2017-03-01 16:24:03 -0500

    Update python uni-tests

--- Commands/CppTestCommand.h
+++ Commands/CppTestCommand.h
@@ -5,63 +5,63 @@
 #ifndef __UI_CPP_TEST_COMMAND_REGISTRY__
 #define __UI_CPP_TEST_COMMAND_REGISTRY__
 
-#include "BaseScriptableCommand.h"
+#include <QString>
+#include <FabricCore.h>
+#include "CommandFactory.h"
 #include "CommandManager.h"
 #include "CommandRegistry.h"
-#include "CommandFactory.h"
-#include <QString>
+#include "BaseScriptableCommand.h"
 
 namespace FabricUI {
 namespace Commands {
 
-class CppTestCommand : public BaseScriptableCommand 
+/**
+  For unit-tests.
+*/
+class CppTestScriptableCommand : public BaseScriptableCommand 
 {
-	/**
-		Use for unit-tests.
-	*/
-
- 	public:
-    CppTestCommand() 
-    	: BaseScriptableCommand()
+  public:
+    CppTestScriptableCommand() 
+      : BaseScriptableCommand()
     {
-  	 	BaseScriptableCommand::declareArg(
-       	"arg_1",
-    		false,
-       	"arg_1_default");
+      BaseScriptableCommand::declareArg(
+        "arg_1",
+        false,
+        "arg_1_default_Cpp");
     }
 
-    virtual ~CppTestCommand() 
+    virtual ~CppTestScriptableCommand() 
     {
     }
  
-    virtual bool canUndo() const
+    virtual bool canUndo()
     {
-    	return true;
+      return true;
     }
  
     virtual bool doIt()
     {
-    	return true;
+      return true;
     }
  
     virtual bool undoIt()
     {
       std::cout << "Undo " << getName().toUtf8().constData() << std::endl;
-    	return true;
+      return true;
     }
  
     virtual bool redoIt()
     {
       std::cout << "Redo " << getName().toUtf8().constData() << std::endl;
-    	return true;
+      return true;
     }
 };
 
-class CppTestMetaCommand : public CppTestCommand 
+class CppTestMetaCommand : public CppTestScriptableCommand 
 {
- 	public:
+  public:
     CppTestMetaCommand() 
-    	: CppTestCommand()
+      : CppTestScriptableCommand()
     {
     }
 
@@ -71,33 +71,154 @@ class CppTestMetaCommand : public CppTestCommand
  
     virtual bool doIt()
     {
-  		QMap<QString, QString> map;
-			map["arg_1"] = "fooCpp";
-    	CommandManager::GetCommandManager()->createCommand("cppTestCommand", map);
-    	CommandManager::GetCommandManager()->createCommand("cppTestCommand", map);
-			return true;
+      QMap<QString, QString> map;
+      map["arg_1"] = "string_Cpp";
+
+      CommandManager::GetCommandManager()->createCommand(
+        "cppTestScriptableCommand", 
+        map);
+
+      CommandManager::GetCommandManager()->createCommand(
+        "cppTestScriptableCommand", 
+        map);
+
+      return true;
     }
 };
 
 class CppToPythonTestCommand
 {
-	public:
-		CppToPythonTestCommand() 
-		{
-		}
+  public:
+    CppToPythonTestCommand()
+    {
+    }
 
-		static void RegisterCppTestCommands() 
-		{
-			CommandFactory<CppTestCommand>::RegisterCommand("cppTestCommand");
-			CommandFactory<CppTestMetaCommand>::RegisterCommand("cppTestMetaCommand");
-		}
+    /// Register C++ commands to create them in python.
+    static void RegisterCppTestCommands() 
+    {
+      CommandFactory<CppTestScriptableCommand>::RegisterCommand("cppTestScriptableCommand");
+      CommandFactory<CppTestMetaCommand>::RegisterCommand("cppTestMetaCommand");
+    }
 
-		static void CreatePythonTestCommands() 
-		{
-			QMap<QString, QString> map;
-			map["arg_1"] = "fooCpp";
-    	CommandManager::GetCommandManager()->createCommand("testScriptableCommand", map);
-		}
+    /// Creates C++ commands from C++.
+    static void CreateCppTestCommands() 
+    {
+      QMap<QString, QString> map;
+      map["arg_1"] = "string_Cpp";
+
+      CommandManager::GetCommandManager()->createCommand(
+        "cppTestScriptableCommand", 
+        map);
+
+      CommandManager::GetCommandManager()->createCommand(
+        "cppTestMetaCommand");
+    }
+
+    /// Creates python commands from C++.
+    static void CreatePythonTestCommands() 
+    {
+      QMap<QString, QString> map;
+      map["arg_1"] = "string_Cpp";
+
+      CommandManager::GetCommandManager()->createCommand(
+        "testScriptableCommand", 
+        map);
+
+      CommandManager::GetCommandManager()->createCommand(
+        "testMetaCommand");
+    }
+
+    /// Register the KL commands in KL.
+    /// see Ext/Builtin/FabbricInterfaces/Commands/KLTestCommands.kl.
+    static void RegisterKLTestCommands() 
+    {
+      try
+      {
+        FabricCore::RTVal CppToKLTestCommand = FabricCore::RTVal::Create(
+          CommandRegistry::GetCommandRegistry()->getFabricClient(), 
+          "CppToKLTestCommand", 
+          0, 
+          0);
+
+        CppToKLTestCommand.callMethod(
+          "", 
+          "registerKLTestCommands", 
+          0, 
+          0);  
+      }    
+
+      catch(FabricCore::Exception &e)
+      {
+        printf(
+          "RegisterKLTestCommands: exception: %s\n", 
+          e.getDesc_cstr());
+      }
+
+      // Synchronise the stack
+      CommandRegistry::GetCommandRegistry()->synchronizeKL();
+    }
+
+    /// Creates KL commands from KL.
+    static void CreateKLTestCommandsInKL() 
+    {
+      try
+      {
+        FabricCore::RTVal CppToKLTestCommand = FabricCore::RTVal::Create(
+          CommandRegistry::GetCommandRegistry()->getFabricClient(), 
+          "CppToKLTestCommand", 
+          0, 
+          0);
+
+        CppToKLTestCommand.callMethod(
+          "", 
+          "createKLTestCommands", 
+          0, 
+          0);  
+      }
+
+      catch(FabricCore::Exception &e)
+      {
+        printf(
+          "CreateKLTestCommandsInKL: exception: %s\n", 
+          e.getDesc_cstr());
+      }
+    }
+
+    /// Creates KL commands from C++.
+    static void CreateKLTestCommands() 
+    {
+      try
+      {
+        CommandManager::GetCommandManager()->createCommand("klTestCommand");
+        CommandManager::GetCommandManager()->createCommand("klTestMetaCommand");
+
+        FabricCore::RTVal strRTVal = FabricCore::RTVal::ConstructString(
+          CommandRegistry::GetCommandRegistry()->getFabricClient(), 
+          "string_Cpp");
+
+        FabricCore::RTVal floatRTVal = FabricCore::RTVal::ConstructFloat32(
+          CommandRegistry::GetCommandRegistry()->getFabricClient(), 
+          4.555f);
+     
+        QMap<QString, QString> args;
+        args["arg_1"] = strRTVal.getJSON().getStringCString();
+        args["arg_2"] = floatRTVal.getJSON().getStringCString();
+
+        CommandManager::GetCommandManager()->createCommand(
+          "klTestScriptableCommand", 
+          args);
+
+        CommandManager::GetCommandManager()->createCommand(
+          "klTestScriptableMetaCommand");
+      }
+
+      catch(FabricCore::Exception &e)
+      {
+        printf(
+          "CreateKLTestCommands: exception: %s\n", 
+          e.getDesc_cstr());
+      }
+    }
 };
 
 } // namespace Commands
