d430c9495e345467cd182a0e44b81ad17a2c2b20
Julien Couet <couet.julien@gmail.com>
2017-03-01 16:24:33 -0500

    Shiboken build

--- shiboken/global_commands.h
+++ shiboken/global_commands.h
@@ -4,8 +4,7 @@
 
 #include <FabricUI/Commands/BaseCommand.h>
 #include <FabricUI/Commands/BaseScriptableCommand.h>
-#include <FabricUI/Commands/BaseScriptableCommand_Python.h>
-#include <FabricUI/Commands/BaseCommandManager.h>
-#include <FabricUI/Commands/BaseCommandManager_Python.h>
+#include <FabricUI/Commands/CommandManager.h>
+#include <FabricUI/Commands/CommandManager_Python.h>
 #include <FabricUI/Commands/CommandRegistry.h>
 #include <FabricUI/Commands/CppTestCommand.h>
