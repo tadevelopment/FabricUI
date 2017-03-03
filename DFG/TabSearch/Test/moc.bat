eb11a37176b818b03f8b263558451de84d276a0c
Olivier Pinon <oPinon@users.noreply.github.com>
2017-03-03 13:21:06 -0500

    Added HeatBar to CMake

--- DFG/TabSearch/Test/moc.bat
+++ DFG/TabSearch/Test/moc.bat
@@ -8,3 +8,4 @@ echo // MOC > %Dir%/Moc.cpp
 %QtDir%/bin/moc %Dir%/../ResultsView.h >> %Dir%/Moc.cpp
 %QtDir%/bin/moc %Dir%/../ResultPreview.h >> %Dir%/Moc.cpp
 %QtDir%/bin/moc %Dir%/../ItemView.h >> %Dir%/Moc.cpp
+%QtDir%/bin/moc %Dir%/../HeatBar.h >> %Dir%/Moc.cpp
