2c1efa4a8cde4c201df32c07b483ff5236d47879
Eric Thivierge <ethivierge@gmail.com>
2017-03-07 11:57:20 -0500

    Linux adjustments

--- KLEditor/LineNumberWidget.cpp
+++ KLEditor/LineNumberWidget.cpp
@@ -58,9 +58,9 @@ void LineNumberWidget::paintEvent(QPaintEvent * event)
   int lineHeight = m_metrics->height();
   int offset = lineHeight;
   if(m_lineOffset == 0)
-    offset += 1;
+    offset -= 2;
   else
-    offset -= 3;
+    offset -= 4;
 #else
   int lineHeight = m_metrics->height() + m_metrics->leading() * 2;
   int offset = lineHeight + 1;
