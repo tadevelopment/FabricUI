637f542999c38b5be837dc48d7bc8c3d12ea74a6
Borja Morales <borja.morales@fabricengine.com>
2017-03-10 13:54:41 +0100

    PUBLISH: FE-8257 Split from preset was not creating an undo command and was breaking the undo stack

--- DFG/DFGWidget.h
+++ DFG/DFGWidget.h
@@ -17,6 +17,7 @@
 #include <FabricUI/DFG/DFGNotificationRouter.h>
 #include <FabricUI/DFG/DFGTabSearchWidget.h>
 #include <FabricUI/DFG/Dialogs/DFGBaseDialog.h>
+#include <FabricUI/DFG/DFGUICmdHandler.h>
 #include <QVBoxLayout>
 
 #include <FTL/OwnedPtr.h>
@@ -860,7 +861,9 @@ namespace DFG {
 
       void onTriggered()
       {
-        m_dfgWidget->splitFromPreset(m_node->name().c_str());
+        DFGController *UIController = m_dfgWidget->getDFGController();        
+        FabricCore::DFGExec &exec  = UIController->getExec().getSubExec(m_node->name().c_str());
+        UIController->getCmdHandler()->dfgDoSplitFromPreset(UIController->getBinding(), m_node->name_QS(), exec);
       }
 
     private:
