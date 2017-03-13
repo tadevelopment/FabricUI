e91b1216cc6667515e1aa0d7654d2c16ddfe4da2
Eric Thivierge <ethivierge@gmail.com>
2017-03-13 09:54:32 -0400

    Set Linux offset to +1 as it was previously when 0 offset

--- KLEditor/LineNumberWidget.cpp
+++ KLEditor/LineNumberWidget.cpp
@@ -65,7 +65,7 @@ void LineNumberWidget::paintEvent(QPaintEvent * event)
   int lineHeight = m_metrics->height() + m_metrics->leading() * 2;
   int offset = lineHeight + 1;
   if(m_lineOffset == 0)
-    offset -= 1;
+    offset += 1;
   else
     offset -= 5;
 #endif
