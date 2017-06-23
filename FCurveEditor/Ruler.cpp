1aba72209ce0a6f380d66036eb3a7a5d3d7f0c66
Olivier Pinon <oPinon@users.noreply.github.com>
2017-06-23 11:19:07 -0400

    Added graduations to the ruler

--- FCurveEditor/Ruler.cpp
+++ FCurveEditor/Ruler.cpp
@@ -2,6 +2,7 @@
 #include <FabricUI/FCurveEditor/Ruler.h>
 
 #include <QPainter>
+#include <QDebug>
 
 using namespace FabricUI::FCurveEditor;
 
@@ -44,16 +45,39 @@ void Ruler::paintEvent( QPaintEvent * e )
 {
   Parent::paintEvent( e );
 
+  if( m_min == m_max )
+    return;
+
+  float bigFactor = std::powf( 2, std::ceil( -std::log2( m_max - m_min ) ) ) * 4;
+  float smallFactor = bigFactor * 8;
+
   // TODO : vertical
   if( m_orientation == Qt::Horizontal )
   {
+
     const QRect r = this->contentsRect();
     QPainter p( this );
-    p.setPen( QColor( 128, 128, 128 ) );
-    for( float i = 0; i < 1; i += 0.1 )
+    QPen pen;
+    pen.setColor( QColor( 128, 128, 128 ) );
+    pen.setWidthF( 4 );
+    p.setPen( pen );
+    for( float i = std::floor( m_min * bigFactor ); i < bigFactor * m_max; i++ )
     {
-      float x = Map( i, m_orientation, m_min, m_max, r );
-      p.drawLine( QPointF( x, r.top() ), QPointF( x, r.bottom() ) );
+      float xs = i / bigFactor;
+      float xw = Map( xs, m_orientation, m_min, m_max, r );
+      float middle = ( r.top() + r.bottom() ) / 2;
+      p.drawLine( QPointF( xw, r.top() ), QPointF( xw, middle ) );
+      p.drawText( QPointF( xw, 0.9 * r.bottom() + 0.1 * r.top() ), QString::number( xs ) );
+    }
+    pen.setWidthF( 1 );
+    p.setPen( pen );
+    for( float i = std::floor( m_min * smallFactor ); i < smallFactor * m_max; i++ )
+    {
+      float xs = i / smallFactor;
+      float xw = Map( xs, m_orientation, m_min, m_max, r );
+      float smallRatio = 0.3;
+      float end = ( 1 - smallRatio ) * r.top() + smallRatio * r.bottom();
+      p.drawLine( QPointF( xw, r.top() ), QPointF( xw, end ) );
     }
   }
 }
